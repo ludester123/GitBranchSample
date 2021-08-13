b8cc75e0c9f6555f3966d3cd565f725447104c43
Dester Lu <ludester123@gmail.com>
2021-08-13 09:26:15 +0800

    add Math Branch

--- GitHubSampleApp/src/GitBranchApp/maniApp.java
+++ GitHubSampleApp/src/GitBranchApp/maniApp.java
@@ -5,6 +5,15 @@ public class maniApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st Version");
+		//這是一個加法運算
+		Math cMath = new Math();
+		System.out.println(cMath.add(10, 5));
 	}
 
+}
+
+class Math{
+	int add(int a,int b) {
+		return a+b;
+	}
 }
